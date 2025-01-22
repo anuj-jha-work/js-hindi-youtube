dff367f5bea86930cfa4018ca10e2044375497a6
ANUJ JHA <anujjha05875@gmail.com>
2025-01-22 15:54:28 +0000

    learned date and time

---
+++ 01_basics/07_date.js
@@ -0,0 +1,8 @@
+//let mydate=new Date()
+//console.log(Date)
+//console.log(mydate.toString())
+//console.log(mydate.toJSON())
+//console.log(mydate.toDateString())
+//console.log(mydate.toTimeString())
+let updatedDate=new Date(2023,0,23)
+console.log(updatedDate)
